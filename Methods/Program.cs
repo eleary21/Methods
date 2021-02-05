7c0dc5e53757a4d72e592ca6c7e1dbcdb969c998
Ethan Leary <eleary@usf.edu>
2021-02-05 16:57:26 -0500

    Method changes

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿//Author: Ethan leary
+//Date: 2/5/2021
+//Description: This program uses a method to display the name of the user
+//after they type their name and click enter.
+using System;
 
 namespace Methods
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Display directions for the user
+            Console.WriteLine("Please Write Your Nameand Click Enter");
+            //Store what the user types into "input"
+            string input = Console.ReadLine();
+            //Display the input to create a message for the user
+            Console.WriteLine("Hello " + input + "!");
+            //Awaits for the user to press a button to end the program
+            Console.ReadKey(true);
+
         }
     }
 }
